bcbc080e54f168d32d4fc50666687f71ce719b57
KharlanEd <eduardxarlan@gmail.com>
2023-02-24 23:12:14 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { GlobalStyle } from "./Globalstyle";
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import data from 'path/data.json'
-import friends from '../path/friends.json'
+
 import { FriendList } from "./Friends/FriendList";
 import { TransactionHistory } from "./Transaction/TransactionHistory";
 import transactions from '../path/transactions.json'
@@ -24,7 +24,7 @@ export const App = () => {
       <Profile />
       <Statistics title="Upload stats"
         stats={data} />
-      <FriendList friends={friends} />
+      <FriendList  />
       <TransactionHistory items={transactions}/>
 
       <GlobalStyle/>
